76f0fb315caf08ca74e176ebfb8e465d086e61ac
sushil kamde <72201008+sushilkamde1@users.noreply.github.com>
2021-10-26 09:06:46 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import { FormEx } from "./FormEx";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h2>React Formik validation</h2>
+      <FormEx />
+    </div>
+  );
+}
